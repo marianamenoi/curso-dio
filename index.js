7168e0eac60d14b4ae3a6c7fce9189cda7d4e4a6
marianamenoi <marianamenoi>
2025-05-15 15:43:55 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("olá mundo");
-console.log("mariana")
+let nome = "homem Aranha";
+let xp = 100000;
+let nivel;
+
+if (xp <=1000) {
+    nivel = "ferro";
+} else if (xp >=1001 && xp <= 2000) {
+    nivel = "bronze";
+} else if (xp >=2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >=5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >=7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >=8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >=9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+
+
